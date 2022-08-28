cb66d6a8509a4b22166d40513be41608743827c6
Yuankai Wang <yuankai4@illinois.edu>
2022-08-28 16:16:39 -0500

    add support for multiple clients

---
+++ mp1_log_searcher/src/SocketMultipleServer.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class SocketMultipleServer {
+    private ServerSocket serverSocket;
+
+    public static void main(String[] args) {
+        SocketMultipleServer server = new SocketMultipleServer();
+        server.start();
+    }
+
+    public void start() {
+        try {
+            serverSocket = new ServerSocket(123);
+            System.out.println("Listening for a connection");
+            while (true) {
+                System.out.println("Received a connection");
+                new SocketServer(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void stop() {
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- mp1_log_searcher/src/SocketServer.java
+++ mp1_log_searcher/src/SocketServer.java
@@ -2,29 +2,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
 
-// TODO: Handle one client for now. Maybe need to handle multiple
-public class SocketServer {
-    private ServerSocket serverSocket;
+public class SocketServer extends Thread {
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
 
-    public static void main(String[] args) {
-        SocketServer server = new SocketServer();
-        server.startServer();
+    public SocketServer(Socket clientSocket) {
+        this.clientSocket = clientSocket;
     }
 
-    public void startServer() {
+    public void run() {
         try {
-            serverSocket = new ServerSocket(123);
-            System.out.println("Listening for a connection");
-            clientSocket = serverSocket.accept();
-            System.out.println("Received a connection");
-
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new PrintWriter(clientSocket.getOutputStream());
 
