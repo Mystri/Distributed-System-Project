39b9bdd4cd61eeb35533ea77461c743814f1522d
Yuankai Wang <yuankai4@illinois.edu>
2022-09-09 13:34:46 -0500

    update script

--- mp1_log_searcher/startClient.sh
+++ mp1_log_searcher/startClient.sh
@@ -1,4 +1,11 @@
-# Start the client. Make sure all the server are running before starting the client.
+# Clean up the directory and prepare for compiling the project. Make sure all the server are running before starting the client.
+rm -f sources.txt
+find ./src -name '*.java' > sources.txt
+rm -rf ./compile
+mkdir ./compile
+# Compile the project
+javac -target 8 -source 8 -d ./compile -classpath ./compile/mp1.jar @sources.txt
+# Start the Client
 # Use locally
 #java -classpath ./compile:./compile/mp1.jar ./src/LogFinderMain.java
 # Use on VM
