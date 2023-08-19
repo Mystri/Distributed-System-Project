a0993e169349ce647f1b814a4a3b3f7c0b60cb6e
Mystri <boyouhan@gmail.com>
2023-08-20 02:06:15 +0800

    initial commit

---
+++ mp3_simple_distributed_file_system/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // setup introducer
+
+        // start clients and server
+
+        // loop
+        // build connection topology by processing received information
+        // start ping thread, with time interval
+        // collect ping responses for next ping
+
+        // exit
+    }
+}
---
+++ mp3_simple_distributed_file_system/startServer.sh
@@ -0,0 +1,3 @@
+# Make sure all the server are running before starting the client.
+# Start the server
+java -classpath ./compile:./compile/mp1.jar SocketMultipleServer
